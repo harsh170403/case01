2af3175d07218c473faa96b00828f65622b909d5
harsh170403 <harshdhattarwal17@gmail.com>
2024-10-16 01:03:51 +0530

    commit

---
+++ week-13/backend/src/middlerware.ts
@@ -0,0 +1,18 @@
+
+export function initMiddleware(app: { use: (arg0: string, arg1: (c: any, next: any) => Promise<any>) => void; }) {
+    app.use('/api/v1/blog/*', async (c: { req: { header: (arg0: string) => string; }; env: { JWT_SECRET: any; }; status: (arg0: number) => void; json: (arg0: { error: string; }) => any; }, next: () => void) => {
+        const header = c.req.header("authorization") || "";
+        // Bearer token => ["Bearer", "token"];
+        const token = header.split(" ")[1]
+        
+        // @ts-ignore
+        const response = await verify(token, c.env.JWT_SECRET)
+        if (response.id) {
+          next()
+        } else {
+          c.status(403)
+          return c.json({ error: "unauthorized" })
+        }
+      })
+      
+}
